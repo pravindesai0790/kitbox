fbc490f25f20b8cd20a362fcd4d7c2adff52aba2
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-07 18:25:43 +0530

    Connected to the sql server and created DB migration

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -8,6 +8,6 @@ namespace Core.Entities
         public required string PictureUrl { get; set; }
         public required string Type { get; set; }
         public required string Brand { get; set; }
-        public int QuantitInStock { get; set; }
+        public int QuantityInStock { get; set; }
     }
 }
---
+++ Infrastructure/Migrations/20260807083924_ColumnNameCorrection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnNameCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantitInStock",
+                table: "Products",
+                newName: "QuantityInStock");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantitInStock");
+        }
+    }
+}
