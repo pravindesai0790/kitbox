ab02aa64ddf4b7b4b76dc9a6e23c6ffcc0f6830b
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-10 12:26:15 +0530

    Added get methods for brands and types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -72,6 +72,18 @@ namespace API.Controllers
             return BadRequest("Problem deleting the product");
         }
 
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+        {
+            return Ok(await repo.GetBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+        {
+            return Ok(await repo.GetTypesAsync());
+        }
+
         private bool ProductExists(int id)
         {
             return repo.ProductExists(id);
--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -6,6 +6,8 @@ namespace Core.Interfaces
     {
         Task<IReadOnlyList<Product>> GetProductsAsync();
         Task<Product?> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<string>> GetBrandsAsync();   
+        Task<IReadOnlyList<string>> GetTypesAsync();
         void AddProduct(Product product);
         void UpdateProduct(Product product);
         void DeleteProduct(Product product);
