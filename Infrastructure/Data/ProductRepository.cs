7a887e85146e0fcaec824d3ed3b89ab5fa0b611b
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-10 13:54:22 +0530

    Added filters and sort functionality to product

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -28,9 +28,24 @@ namespace Infrastructure.Data
             return await context.Products.FindAsync(id);
         }
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort)
         {
-            return await context.Products.ToListAsync();
+            var query = context.Products.AsQueryable();
+
+            if(!string.IsNullOrWhiteSpace(brand))
+                query = query.Where(x => x.Brand == brand);
+
+            if(!string.IsNullOrWhiteSpace(type))
+                query = query.Where(x => x.Type == type);
+
+            query = sort switch
+            {
+                "priceAsc" => query.OrderBy(x => x.Price),
+                "priceDesc" => query.OrderByDescending(x => x.Price),
+                _ => query.OrderBy(x => x.Name)
+            };
+
+            return await query.ToListAsync();
         }
 
         public async Task<IReadOnlyList<string>> GetTypesAsync()
