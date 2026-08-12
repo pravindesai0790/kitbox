f9d224607ddeb47441daeee9c47f141aec5f261b
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-12 22:05:23 +0530

    Added search functionllity

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -6,6 +6,7 @@ namespace Core.Specifications
     public class ProductSpecification : BaseSpecification<Product>
     {
         public ProductSpecification(ProductSpecParams specParams) : base(x =>
+            (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
             (!specParams.Brands.Any() || specParams.Brands.Contains(x.Brand)) &&
             (!specParams.Types.Any() || specParams.Types.Contains(x.Type)))
         {
