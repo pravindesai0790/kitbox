1f794cfb45d7a36e917548445b8dee3fbe566bbe
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-17 20:10:15 +0530

    Added routes in application

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
-  ],
+    MatBadge,
+    RouterLink,
+    RouterLink,
+    RouterLinkActive
+],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
