edc1bfe0eecb30ef7dc85d5a5afa01880bf926e4
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-20 11:18:18 +0530

    Displaying items in the cart page and added all functionlity in cart page

---
+++ client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, input } from '@angular/core';
+import { CartItem } from '../../../shared/models/cart';
+import { RouterLink } from "@angular/router";
+import { MatIcon } from "@angular/material/icon";
+import { CurrencyPipe } from '@angular/common';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { CartService } from '../../../core/services/cart.service';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [
+    RouterLink,
+    MatButton,
+    MatIconButton,
+    MatIcon,
+    CurrencyPipe
+],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.css',
+})
+export class CartItemComponent {
+  item = input.required<CartItem>();
+  cartService = inject(CartService);
+
+  incrementQuantity() {
+    this.cartService.addItemToCart(this.item());
+  }
+
+  decrementQuantity() {
+    this.cartService.removeItemFromCart(this.item().productId);
+  }
+
+  removeItemFromCart() {
+    this.cartService.removeItemFromCart(this.item().productId, this.item().quantity);
+  }
+}
