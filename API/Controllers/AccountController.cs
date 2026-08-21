de8221418c12650c455fac4df3ed596dacfaff73
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-21 23:22:29 +0530

    Added a user address class and endpoint to add or update the address

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,7 +5,6 @@ using Core.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -24,7 +23,15 @@ namespace API.Controllers
 
             var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
 
-            if(!result.Succeeded) return BadRequest(result.Errors);
+            if(!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return ValidationProblem();
+            }
 
             return Ok();
         }
@@ -43,13 +50,14 @@ namespace API.Controllers
         {
             if(User.Identity?.IsAuthenticated == false) return NoContent();
 
-            var user = await signInManager.UserManager.GetUserByEmail(User);
+            var user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
 
             return Ok(new
             {
                 user.FirstName,
                 user.LastName,
-                user.Email
+                user.Email,
+                Address = user.Address?.ToDto()
             });
         }
 
@@ -57,6 +65,28 @@ namespace API.Controllers
         public ActionResult GetAuthState()
         {
             return Ok(new { IsAuthenticated = User.Identity?.IsAuthenticated ?? false });
+        }
+
+        [Authorize]
+        [HttpPost("address")]
+        public async Task<ActionResult<Address>> CreateOrUpdateAddress(AddressDto addressDto)
+        {
+            var user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
+
+            if (user.Address == null)
+            {
+                user.Address = addressDto.ToEntity();
+            } 
+            else
+            {
+                user.Address.UpdateFromDto(addressDto);
+            }
+
+            var result = await signInManager.UserManager.UpdateAsync(user);
+
+            if (!result.Succeeded) return BadRequest("Problem updating user address");
+
+            return Ok(user.Address.ToDto());
         }
     }
 }
--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -16,6 +16,16 @@ namespace API.Extensions
             return userToReturn;
         }
 
+        public static async Task<AppUser> GetUserByEmailWithAddress(this UserManager<AppUser> userManager, ClaimsPrincipal user)
+        {
+            var userToReturn = await userManager.Users
+                .Include(x => x.Address)
+                .FirstOrDefaultAsync(x => x.Email == user.GetEmail()) 
+                ?? throw new AuthenticationException("User not found");
+                
+            return userToReturn;
+        }
+
         public static string GetEmail(this ClaimsPrincipal user)
         {
             var email = user.FindFirstValue(ClaimTypes.Email) 
