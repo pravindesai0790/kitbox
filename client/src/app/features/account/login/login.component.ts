37430bfd22e5c0a967acc15932916f3d9507abdf
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-24 18:23:43 +0530

    Created login form

--- client/src/app/features/account/login/login.component.ts
+++ client/src/app/features/account/login/login.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatInput } from '@angular/material/input';
+import { MatFormField, MatLabel } from '@angular/material/select';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCard,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatButton
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private accountService = inject(AccountService);
+  private roter = inject(Router);
+
+  loginFrom = this.fb.group({
+    email: [''],
+    password: ['']
+  });
+
+  onSubmit() {
+    this.accountService.login(this.loginFrom.value).subscribe({
+      next: () => {
+        this.accountService.getUserInfo();
+        this.roter.navigateByUrl('/shop');
+      }
+    })
+  }
+}
