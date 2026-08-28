9b1c729948f20b5a366872430c3f5eee3d118598
pravindesai0790 <pravindesai0790@gmail.com>
2026-08-28 11:39:47 +0530

    Created deliverymethods entity and migration with seed data in API

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -7,7 +7,7 @@ namespace Infrastructure.Data
     {
         public static async Task SeedAsync(StoreContext context)
         {
-            if(!context.Products.Any())
+            if (!context.Products.Any())
             {
                 var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
 
@@ -16,6 +16,19 @@ namespace Infrastructure.Data
                 if(products == null) return;
 
                 context.AddRange(products);
+
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.DeliveryMethods.Any())
+            {
+                var dmData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/delivery.json");
+
+                var deliveryMethods = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
+
+                if(deliveryMethods == null) return;
+
+                context.AddRange(deliveryMethods);
 
                 await context.SaveChangesAsync();
             }
